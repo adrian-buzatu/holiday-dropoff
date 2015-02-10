44c431edf13da990b58207ca94427756874306cf
b_adrian <adrian.t.buzatu@gmail.com>
2015-02-11 01:04:30 +0200

    -m

---
+++ applications/front/controllers/Homepage.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This is the controller for the homepage
+ */
+
+/**
+ * Description of Homepage
+ *
+ * @author adrian.t.buzatu@gmail.com
+ */
+class Homepage {
+    //put your code here
+}
