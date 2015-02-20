cf484fd59da3c79e75e658d01206e64689ebae8e
b_adrian <adrian.t.buzatu@gmail.com>
2015-02-20 03:17:58 +0200

    added new libraries

--- applications/admin/views/layout_main.php
+++ applications/admin/views/layout_main.php
@@ -6,8 +6,13 @@
         <title>Holiday Drop Off Camps - Admin Panel</title>
         <!--<link href="stylesheet.css" type="text/css" rel="stylesheet" />-->
         <link href="<?php echo asset_url()?>css/style.css" rel="stylesheet" type="text/css" />
+        <script src="<?php echo asset_url();?>js/jquery-1.11.2.min.js" type="text/javascript"></script>
+        <script src="<?php echo asset_url();?>js/main.js" type="text/javascript"></script>
+        <?php if(controller() == 'pages'):?>
+            <script src="<?php echo asset_url();?>js/nicEdit.js" type="text/javascript"></script>
+        <?php endif;?>
     </head>
-    <body>
+    <body id="<?php echo controller();?>" rel="<?php echo asset_url()?>">
         <div id="wrapper">
             <div id="wrap">
                 <div id="page"><div class="logo"><img src="<?php echo asset_url()?>images/logo.png" alt="" /></div>
@@ -18,38 +23,38 @@
                                 <div class="menutop">
                                     <ul class="mainmenu">
                                         <div class="top_heading_red">Our Camps</div>
-                                        <li><a class=""  href="maincamp.php">Camp Groups</a></li>
-                                        <li><a class=""  href="camp.php">Camps</a></li>
-                                        <li><a class=""  href="pages.php?pageID=12">What happens</a></li>
-                                        <li><a class=""  href="pages.php?pageID=13">Sports at HDO</a></li>
-                                        <li><a class=""  href="pages.php?pageID=14">Facilities</a></li>
-                                        <li><a class=""  href="pages.php?pageID=15">Camp Dates</a></li>
-                                        <li><a class=""  href="pages.php?pageID=16">Staff at HDO</a></li>
-                                        <li><a class=""  href="pages.php?pageID=16">Work For Us</a></li>
+                                        <li><a class=""  href="#">Camp Groups</a></li>
+                                        <li><a class=""  href="#">Camps</a></li>
+                                        <li><a class="<?php echo item() == 'what-happens' ? 'active' : '';?>"  href="<?php echo base_url()?>pages/edit/what-happens">What happens</a></li>
+                                        <li><a class="<?php echo item() == 'sports-at-hdo' ? 'active' : '';?>"  href="<?php echo base_url()?>pages/edit/sports-at-hdo">Sports at HDO</a></li>
+                                        <li><a class="<?php echo item() == 'facilities' ? 'active' : '';?>"  href="<?php echo base_url()?>pages/edit/facilities">Facilities</a></li>
+                                        <li><a class="<?php echo item() == 'camp-dates' ? 'active' : '';?>"  href="<?php echo base_url()?>pages/edit/camp-dates">Camp Dates</a></li>
+                                        <li><a class="<?php echo item() == 'staff-at-hdo' ? 'active' : '';?>"  href="<?php echo base_url()?>pages/edit/staff-at-hdo">Staff at HDO</a></li>
+                                        <li><a class="<?php echo item() == 'work-for-us' ? 'active' : '';?>"  href="<?php echo base_url()?>pages/edit/work-for-us">Work For Us</a></li>
                                         <div class="top_heading_red">Pages</div>
-                                        <li><a class="active" href="home.php">Home</a></li>
-                                        <li><a class="" href="pages.php?pageID=17">Download Our HDO E-Flyer</a></li>
-                                        <li><a class="" href="pages.php?pageID=1">About US</a></li>
-                                        <li><a class="" href="pages.php?pageID=2">Useful Stuff</a></li>
-                                        <li><a class="" href="pages.php?pageID=3">Privacy Policy</a></li>
-                                        <li><a class="" href="pages.php?pageID=4">Terms and conditions</a></li>
-                                        <li><a class="" href="pages.php?pageID=5">Work for Us</a></li>
-                                        <li><a class="" href="pages.php?pageID=7">Holiday Drop Off Camps</a></li>
-                                        <li><a class="" href="pages.php?pageID=6">Sitemap</a></li>
-                                        <li><a class="" href="pages.php?pageID=18">Prices</a></li>
-                                        <li><a class="" href="news.php">News</a></li>
+                                        <li><a class="<?php echo item() == 'home' ? 'active' : '';?>" href="#">Home</a></li>
+                                        <li><a class="" href="#">Download Our HDO E-Flyer</a></li>
+                                        <li><a class="<?php echo item() == 'about-us' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/about-us">About US</a></li>
+                                        <li><a class="<?php echo item() == 'useful-stuff' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/useful-stuff">Useful Stuff</a></li>
+                                        <li><a class="<?php echo item() == 'privacy-policy' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/privacy-policy">Privacy Policy</a></li>
+                                        <li><a class="<?php echo item() == 'terms-and-conditions' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/terms-and-conditions">Terms and conditions</a></li>
+                                        <li><a class="<?php echo item() == 'work-for-us' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/work-for-us">Work for Us</a></li>
+                                        <li><a class="<?php echo item() == 'holyay-drop-off-camps' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/holyay-drop-off-camps">Holiday Drop Off Camps</a></li>
+                                        <li><a class="<?php echo item() == 'sitemap' ? 'active' : '';?>" href="<?php echo base_url()?>pages/edit/sitemap">Sitemap</a></li>
+                                        <li><a class="" href="<?php echo base_url()?>pages/edit/privacy-policy">Prices</a></li>
+                                        <li><a class="" href="#">News</a></li>
                                         <div class="top_heading_red">Misc</div>
-                                        <li><a class="" href="homebanners.php">Home Banners</a></li>
-                                        <li><a class="" href="gallery_category.php">Gallery Categories</a></li>
-                                        <li><a class="" href="banners.php">My Photos</a></li>
-                                        <li><a class="" href="video.php">My Video</a></li>
-                                        <li><a class="" href="order.php">Order</a></li>
-                                        <li><a class="" href="coupon.php">Coupon</a></li>
-                                        <li><a class="" href="testimonial.php">Testimonial</a></li>              
-                                        <li><a class="" href="user.php">Users</a></li>
-                                        <li><a class="" href="downloadCSV.php">Download CSV</a></li>
-                                        <li><a href="changepasword.php">Change Password</a></li>
-                                        <li><a href="actions.php?action=logout">Logout</a></li>
+                                        <li><a class="" href="#">Home Banners</a></li>
+                                        <li><a class="" href="#">Gallery Categories</a></li>
+                                        <li><a class="" href="#">My Photos</a></li>
+                                        <li><a class="" href="#">My Video</a></li>
+                                        <li><a class="" href="#">Order</a></li>
+                                        <li><a class="" href="#">Coupon</a></li>
+                                        <li><a class="" href="#">Testimonial</a></li>              
+                                        <li><a class="" href="#">Users</a></li>
+                                        <li><a class="" href="#">Download CSV</a></li>
+                                        <li><a href="#">Change Password</a></li>
+                                        <li><a href="#">Logout</a></li>
                                     </ul>
                                 </div>
                             </div>
