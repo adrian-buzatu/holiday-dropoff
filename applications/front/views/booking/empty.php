c2c0af8577fb5c7dc158a7c2d5cb62b070763596
b_adrian <adrian.t.buzatu@gmail.com>
2015-11-14 23:34:02 +0200

    updates

---
+++ applications/front/views/booking/empty.php
@@ -0,0 +1,13 @@
+<div class="bookonline-right-content">
+    
+    <div class="bookonline-heading-text"><img src="<?php echo asset_url() ?>images/book-online-img.png" alt="" />
+        <p>You are signed in as <?php echo $user['user'] ?></p>
+        <span>
+            <a href="<?php echo base_url() ?>logout"><img src="<?php echo asset_url() ?>images/signoutbt.png" alt="" /></a></span></div>
+    <div class="bookonline-middle-content">
+        There are no camps available right now.
+    </div>
+    
+    <div class="c1"></div>
+</div>
+
